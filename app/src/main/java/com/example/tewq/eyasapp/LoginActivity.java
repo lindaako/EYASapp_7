be60d577ae5e672381c5f989921ba05a97055c59
lindaako <danielleleeveisella@gmail.com>
2018-10-29 16:19:49 +0900

    done

--- app/src/main/java/com/example/tewq/eyasapp/LoginActivity.java
+++ app/src/main/java/com/example/tewq/eyasapp/LoginActivity.java
@@ -80,4 +80,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void clickexit(View v)
+    {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
 }
