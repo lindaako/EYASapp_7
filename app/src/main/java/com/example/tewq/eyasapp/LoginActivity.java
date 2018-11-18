1627cb1d19cd1b2a45e25d7e71c8111387d12db5
linda <danielleleeveisella@gmail.com>
2018-11-18 22:33:37 +0900

    email check for password reset done!

--- app/src/main/java/com/example/tewq/eyasapp/LoginActivity.java
+++ app/src/main/java/com/example/tewq/eyasapp/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.example.tewq.eyasapp;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -27,31 +32,62 @@ public class LoginActivity extends AppCompatActivity {
         Button bLogin, bRegister;
         EditText etUsername, etPassword;
         TextView etForgotPassword;
+        EditText edittext;
         //private static String URL  ="https://youngashly.000webhostapp.com/login.php";
         private static String URL ="http://eyas.dx.am/login.php";
+        private static String URL2 ="http://eyas.dx.am/emailcheck.php";
         private Snackbar snackbar;
         private ProgressDialog pd;
+        private ProgressDialog em;
+        private AlertDialog.Builder alert;
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_login);
-
+            final Context mcontext=this;
             etForgotPassword = findViewById(R.id.etForgotPassword);
             etUsername =  findViewById(R.id.etUsername);
             etPassword =  findViewById(R.id.etPassword);
             pd = new ProgressDialog(LoginActivity.this);
+            em = new ProgressDialog(LoginActivity.this);
             bLogin = findViewById(R.id.bLogin);
             bRegister = findViewById(R.id.bRegister);
+
 
             etForgotPassword.setOnClickListener(new View.OnClickListener()
             {
                 @Override
                 public void onClick(View v) {
 
-                    Intent forgotpassIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-                    LoginActivity.this.startActivity(forgotpassIntent);
+                    alert = new AlertDialog.Builder(mcontext);
+                    edittext = new EditText(LoginActivity.this);
+                    alert.setMessage("Enter the email address associated with your account to get a verification code");
+                    alert.setTitle("Forgot password ");
+                    alert.setView(edittext);
+                    alert.setPositiveButton("Continue", new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int whichButton)
+                        {
+                            emailcheck();
+
+                        }
+                    });
+
+                    alert.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int whichButton)
+                        {
+                            dialog.cancel();
+                        }
+                    });
+
+                    alert.show();
+
+
                 }
             });
 
@@ -75,14 +111,69 @@ public class LoginActivity extends AppCompatActivity {
 
         }
 
-        private void loginRequest(){
+        private void emailcheck ()
+        {
+            em.setMessage("Checking for existence . . .");
+            em.show();
+            RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+
+            String response2 = null;
+            final String finalResponse2 = response2;
+            StringRequest postRequest = new StringRequest(Request.Method.POST,URL2,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response2) {
+
+                            em.hide();
+                            showSnackbar(response2);
+
+                            if(response2.equals("Email exists"))
+                            {
+
+                                startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class));
+
+                            }
+                            else{}
+
+                        }
+
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                            // error
+                            em.hide();
+                            Log.d("ErrorResponse", finalResponse2);
+
+
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    params.put("email", edittext.getText().toString());
+                    return params;
+
+                }
+            };
+            postRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            queue.add(postRequest);
+
+        }
+
+
+    private void loginRequest(){
             pd.setMessage("Signing In . . .");
             pd.show();
             RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-            String response = null;
 
-            final String finalResponse = response;
-
+        String response = null;
+        final String finalResponse = response;
             StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
                     new Response.Listener<String>()
                     {
@@ -128,6 +219,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
         }
+
+
         public void showSnackbar(String stringSnackbar){
             snackbar.make(findViewById(android.R.id.content), stringSnackbar.toString(), Snackbar.LENGTH_SHORT)
                     .setActionTextColor(getResources().getColor(R.color.colorPrimary))
