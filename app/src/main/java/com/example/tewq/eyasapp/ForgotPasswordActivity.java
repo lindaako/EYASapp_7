84ddb245357e31cd27c6d783d61effa0a771c16f
linda <danielleleeveisella@gmail.com>
2018-11-25 01:33:20 +0900

    Password reset code detected by app

--- app/src/main/java/com/example/tewq/eyasapp/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/tewq/eyasapp/ForgotPasswordActivity.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,18 +34,19 @@ public class ForgotPasswordActivity extends AppCompatActivity
 {
     LoginActivity.Singleton pass_reset=LoginActivity.Singleton.getInstance();
     final String passwordResetCodes = pass_reset.getCodeString();
-    private static String URL ="http://eyas.dx.am/login.php";
+    private static String URL ="http://eyas.dx.am/Resetpassword.php";
     LoginActivity.Singleton e_mail=LoginActivity.Singleton.getInstance();
     final String user_email = e_mail.getString();
     EditText etEmail, etPassResetCode,etNewPassword;
     Button bResetPassword;
-
+    private Snackbar snackbar;   private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        pd = new ProgressDialog(ForgotPasswordActivity.this);
         etEmail =  findViewById(R.id.etEmail);
         etEmail.setText(user_email);
 
@@ -53,31 +55,119 @@ public class ForgotPasswordActivity extends AppCompatActivity
         bResetPassword = findViewById(R.id.bResetPassword);
 
 
-        bResetPassword.setOnClickListener(new View.OnClickListener()
-        {
+        bResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String mail = etEmail.getText().toString();
                 String PRcode = etPassResetCode.getText().toString();
                 String passwordResetCode = String.valueOf(passwordResetCodes);
+                String new_pass = etNewPassword.getText().toString();
 
-        if ((passwordResetCode.equals(PRcode)))
-        {
-            Toast.makeText(getApplicationContext(), "The codes match!", Toast.LENGTH_SHORT).show();
-        }
+                if (!((PRcode.equals("")) || (mail.equals("")) || (new_pass.equals(""))))
+                {
 
-        else
-            {
-                Toast.makeText(getApplicationContext(), "The codes do not match,try again!", Toast.LENGTH_SHORT).show();
+                    if ((passwordResetCode.equals(PRcode)))
+                    {
+                        //Toast.makeText(getApplicationContext(), "The codes match!", Toast.LENGTH_SHORT).show();
+                        UpdatePassword();
+
+                    }
+
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "The codes do not match,try again!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Please fill in all the required fields!", Toast.LENGTH_SHORT).show();
+                }
+
             }
-            }
+
 
         });
     }
 
 
 
+    private void UpdatePassword(){
+        pd.setMessage("Changing password . . .");
+        pd.show();
+        RequestQueue queue = Volley.newRequestQueue(ForgotPasswordActivity.this);
+        String response = null;
+        final String finalResponse = response;
 
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>()
+                {
+
+                    @Override
+                    public void onResponse(String response) {
+                        pd.hide();
+                        //Response
+                        showSnackbar(response);
+
+                        if(response.equals("Password changed successfully"))
+                        {
+                            new CountDownTimer(2000,1000)
+                            {
+                                public void onFinish()
+                                {
+                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                }
+
+                                public void onTick(long millisUntilFinished)
+                                {
+
+                                }
+
+                            }.start();
+
+
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("ErrorResponse", finalResponse);
+
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+
+                params.put("email", etEmail.getText().toString());
+                params.put("password", etNewPassword.getText().toString());
+
+                return params;
+            }
+        };
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(postRequest);
+
+    }
+
+    public void showSnackbar(String stringSnackbar){
+        snackbar.make(findViewById(android.R.id.content), stringSnackbar.toString(), Snackbar.LENGTH_SHORT)
+                .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                .show();
+    }
 
 
 
